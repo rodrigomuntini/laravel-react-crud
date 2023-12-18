de87f647f3b77e492f211631b3539437eec07464
rodrigo muntini <digomuntini@hotmail.com>
2023-12-17 22:59:11 -0300

    project

---
+++ react/src/views/Login.jsx
@@ -0,0 +1,52 @@
+import {Link} from "react-router-dom";
+import axiosClient from "../axios-client.js";
+import {createRef} from "react";
+import {useStateContext} from "../context/ContextProvider.jsx";
+import { useState } from "react";
+
+export default function Login() {
+  const emailRef = createRef()
+  const passwordRef = createRef()
+  const { setUser, setToken } = useStateContext()
+  const [message, setMessage] = useState(null)
+
+  const onSubmit = ev => {
+    ev.preventDefault()
+
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    }
+    axiosClient.post('/login', payload)
+      .then(({data}) => {
+        setUser(data.user)
+        setToken(data.token);
+      })
+      .catch((err) => {
+        const response = err.response;
+        if (response && response.status === 422) {
+        }
+      })
+  }
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Login</h1>
+
+          {message &&
+            <div className="alert">
+              <p>{message}</p>
+            </div>
+          }
+
+          <input ref={emailRef} type="email" placeholder="Email"/>
+          <input ref={passwordRef} type="password" placeholder="Senha"/>
+          <button className="btn btn-block">Login</button>
+          <p className="message">Não esta registrado? <Link to="/signup">Crie uma conta</Link></p>
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ react/src/views/Signup.jsx
@@ -0,0 +1,57 @@
+import {Link} from "react-router-dom";
+import {createRef, useState} from "react";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../context/ContextProvider.jsx";
+
+export default function Signup() {
+  const nameRef = createRef()
+  const emailRef = createRef()
+  const passwordRef = createRef()
+  const passwordConfirmationRef = createRef()
+  const {setUser, setToken} = useStateContext()
+  const [errors, setErrors] = useState(null)
+
+  const onSubmit = ev => {
+    ev.preventDefault()
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    }
+    axiosClient.post('/signup', payload)
+      .then(({data}) => {
+        setUser(data.user)
+        setToken(data.token);
+      })
+      .catch(err => {
+        const response = err.response;
+        if (response && response.status === 422) {
+        }
+      })
+  }
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Signup</h1>
+          {errors &&
+            <div className="alert">
+              {Object.keys(errors).map(key => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          }
+          <input ref={nameRef} type="text" placeholder="Nome"/>
+          <input ref={emailRef} type="email" placeholder="Email "/>
+          <input ref={passwordRef} type="password" placeholder="Senha"/>
+          <input ref={passwordConfirmationRef} type="password" placeholder="Confirme senha"/>
+          <button className="btn btn-block">Signup</button>
+          <p className="message">Ja tem cadastro? <Link to="/login">Faça o login</Link></p>
+        </form>
+      </div>
+    </div>
+  )
+}
